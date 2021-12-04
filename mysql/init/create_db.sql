bc9994db55942eff0fe155f32c4d02f013ae3252
shota-aa <dqx1005@icloud.com>
2021-12-04 18:32:44 +0900

    change branch from here

--- mysql/init/create_db.sql
+++ mysql/init/create_db.sql
@@ -3,8 +3,20 @@ CREATE DATABASE 21hack18;
 USE 21hack18;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(32) NOT NULL UNIQUE,
-  `hashedPass` VARCHAR(200) NOT NULL,
+  `username` varchar(15) NOT NULL UNIQUE,
+  `hashedPass` varchar(200) NOT NULL,
   -- `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
   PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS 'palaces' (
+  'id' char(36) NOT NULL,
+  'name' varchar(20) NOT NULL,
+  'image' VARBINARY,
+  'createdBy' char(36), NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS 'palace_pins' (
+  'palace' char(36), NOT NULL,
+  'pin' char(36), NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ router/palace.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+
+)
+
+type RequestPalace struct {
+	ID UUID.uuid
+    Name string
+    Image File
+    Pins Pin[]
+    // overallMemo:string(必要かわからん),
+    CreatedBy:uuid
+}
+
+func postPalace() error
