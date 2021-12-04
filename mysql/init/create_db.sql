02aa01ca12e8e074ae2892fde3c2b223bad9daf5
kamecha <masakikameyama81@gmail.com>
2021-12-04 19:21:32 +0900

    暫定テーブル定義完成 画像の大きさは適当

--- mysql/init/create_db.sql
+++ mysql/init/create_db.sql
@@ -2,21 +2,41 @@ DROP DATABASE IF EXISTS 21hack18;
 CREATE DATABASE 21hack18;
 USE 21hack18;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `username` varchar(15) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS `palaces` (
+  `id` char(36) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `image` VARBINARY(100),
+  `createdBy` char(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `pin` (
+  `id` char(36) NOT NULL,
+  `x` int NOT NULL,
+  `y` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `palace_pins` (
+  `palace` char(36) NOT NULL,
+  `pin` char(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `word` (
+  `id` char(36) NOT NULL,
+  `pin` char(36) NOT NULL,
+  `word` varchar(36) NOT NULL,
+  `memo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `palace_words` (
+  `palace` char(36) NOT NULL,
+  `word` char(36) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `user` (
+ `id` char(36) NOT NULL UNIQUE,
+  `username` varchar(15) NOT NULL,
   `hashedPass` varchar(200) NOT NULL,
-  -- `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-  PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS 'palaces' (
-  'id' char(36) NOT NULL,
-  'name' varchar(20) NOT NULL,
-  'image' VARBINARY,
-  'createdBy' char(36), NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS 'palace_pins' (
-  'palace' char(36), NOT NULL,
-  'pin' char(36), NOT NULL
+  `palaceid` char(36) NOT NULL,
+  `mypalaceid` char(36) NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
