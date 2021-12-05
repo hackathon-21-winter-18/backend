177cc2d6593517ed5e064fcdfd8e05fa53ce1a2f
shota-aa <dqx1005@icloud.com>
2021-12-05 11:32:19 +0900

    fix swagger

--- mysql/init/create_db.sql
+++ mysql/init/create_db.sql
@@ -8,15 +8,3 @@ CREATE TABLE IF NOT EXISTS `users` (
   -- `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS 'palaces' (
-  'id' char(36) NOT NULL,
-  'name' varchar(20) NOT NULL,
-  'image' VARBINARY,
-  'createdBy' char(36), NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS 'palace_pins' (
-  'palace' char(36), NOT NULL,
-  'pin' char(36), NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
