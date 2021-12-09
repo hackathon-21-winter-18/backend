8370e6a1a9c5efd0bde43bb7d3e74f803d9b4ae0
shota-aa <dqx1005@icloud.com>
2021-12-09 21:03:18 +0900

    AllowMethods:

--- router/router.go
+++ router/router.go
@@ -17,9 +17,9 @@ func SetRouting(sess sess.Session) {
 	e := echo.New()
 	e.Use(session.Middleware(sess.Store()))
 	e.Use(middleware.Logger())
-	e.Use(middleware.CORS())
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 	}))
 
