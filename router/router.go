929bee6ab7940c288359bd10f847dc41ae2a5abc
hackathon-21-winter-18 <github@acitons>
2021-12-30 21:40:01 +0900

    edit cors

--- router/router.go
+++ router/router.go
@@ -19,7 +19,7 @@ func SetRouting(sess sess.Session) {
 	e.Use(middleware.Logger())
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"https://hackathon-21-winter-18.github.io", "http://localhost:3000"},
+		AllowOrigins:     []string{"https://hackathon-21-winter-18.github.io", "http://localhost:3000", "https://frontend-opal-delta-19.vercel.app"},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowCredentials: true,
 	}))
