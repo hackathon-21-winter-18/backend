6431d08f78b2954259c64d33c0b2e3d8d16dbf56
shota-aa <dqx1005@icloud.com>
2022-01-03 11:25:56 +0900

    make Getnotices

--- model/notices.go
+++ model/notices.go
@@ -2,28 +2,48 @@ package model
 
 import (
 	"context"
-	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 )
 
-type notice struct {
-	ID uuid.UUID
-	User uuid.UUID 
-	Content string
-	Read bool
+type Notice struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	Content   string    `json:"content" db:"content"`
+	Read      bool      `json:"read" db:"read"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
-func CreateNotice(ctx context.Context, editer, creater uuid.UUID, objectID uuid.UUID, palaceIssued bool) error {
+func GetNotices(ctx context.Context, userID uuid.UUID) ([]Notice, error) {
+	var notices []Notice
+	err := db.SelectContext(ctx, &notices, "SELECT id, content, read, created_at FROM notices WhERE userID=? ORDER BY createdBy DESC ", userID)
+	if err != nil {
+		//TODO return userID not found
+		return nil, err
+	}
+
+	for _, notice := range notices {
+		if !notice.Read {
+			_, err = db.ExecContext(ctx, "UPDATE notices SET read=? ", true)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return notices, nil
+}
+
+func CreateNotice(ctx context.Context, createrID uuid.UUID, objectID uuid.UUID, palaceIssued bool) error {
 	noticeID := uuid.New()
 	content := "公開したものを元に他のユーザーが新たな"
 	if palaceIssued {
-		content += fmt.Sprintf("[宮殿]`(https://frontend-opal-delta-19.vercel.app/~/%s)", objectID)
+		content += "宮殿"
 	} else {
-		content += fmt.Sprintf("[テンプレート]`(https://frontend-opal-delta-19.vercel.app/~/%s)", objectID)
+		content += "テンプレート"
 	}
-	content += "を作成しました。" + "\n" + "\n"
-	_, err := db.ExecContext(ctx, "INSERT INTO notices (id, userID, content) VALUES (?, ?, ?) ", noticeID, creater, content)
+	content += "を公開しました。"
+	_, err := db.ExecContext(ctx, "INSERT INTO notices (id, userID, content) VALUES (?, ?, ?) ", noticeID, createrID, content)
 	if err != nil {
 		return err
 	}
